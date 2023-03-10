ec4e0f1cd67c15180f3119a97689166d49ebe223
SeoMoonk <sunghoon1216@naver.com>
2023-03-10 15:35:21 +0900

    세팅2

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,12 @@
+package com.ll;
+
+public class Calc {
+
+    public static int run(String exp)
+    {
+        if(exp.equals("2 + 1"))
+        {
+
+        }
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,9 +2,9 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-
-
         //목표 : 3 * 1 + (1 - (4 * 1 - (1 - 1)))
+
+
 
 
     }
