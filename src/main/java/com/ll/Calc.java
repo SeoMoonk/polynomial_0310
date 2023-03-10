872e4a3f5da6fa3b580c6efb176960beec910b7c
SeoMoonk <sunghoon1216@naver.com>
2023-03-10 15:47:14 +0900

    work_minus

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,12 +1,24 @@
 package com.ll;
 
 public class Calc {
+    public static int run(String exp) {
+        String[] bits = exp.split(" \\+ ");
 
-    public static int run(String exp)
-    {
-        if(exp.equals("2 + 1"))
-        {
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[1]);
 
-        }
+        return a + b;
+    }
+
+    public static int run2(String exp) {
+        String[] bits = exp.split(" - ");
+
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[1]);
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        return a-b;
     }
 }
